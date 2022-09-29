8313178c79fb34da5746d08c9e2ad85b6da90b97
dikiogres <dikiogres@gmail.com>
2022-09-29 16:55:04 +0700

    121 best-time-to-buy-and-sell-stock 29/09/2022

---
+++ leetcode/121 best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+Class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minValue = prices[0];
+        int maxDiff = 0;
+        
+        for (int i = 1; i < prices.size(); i++) {
+            minValue = min(minValue, prices[i]);
+            maxDiff = max(maxDiff, prices[i] - minValue);
+        }
+        
+        return maxDiff;
+    }
+};
