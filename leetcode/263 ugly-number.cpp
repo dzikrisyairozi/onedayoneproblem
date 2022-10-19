1d213b666c95295f7bc5692cd736a6e4b6e79fa9
Dzikri Syairozi <85748069+dikiogres@users.noreply.github.com>
2022-10-19 23:47:23 +0700

    263 ugly-number 10/19/2022

---
+++ leetcode/263 ugly-number.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isUgly(int num){
+        if (num <= 0) return false;
+        while(num % 3 == 0) num /= 3;
+        while(num % 5 == 0) num /= 5;
+        return (num  & (num - 1)) == 0;
+    }
+};
